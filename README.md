4f660b8516295b2300a0615ee62e4244aeb0618f
Kelly Dwan <kelly.dwan@gmail.com>
2017-03-29 13:26:25 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+WordCamp Boston 2017 Theme
+==========================
+
+This is the theme used for WordCamp Boston's website. The compiled CSS is loaded by the boston.wordcamp.org site, while the child theme itself can be used in a local development environment.
---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Enqueue the parent theme's styles.
+ * You can leave this out if you're replacing the parent theme's CSS.
+ */
+function boston_2017_styles() {
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'boston_2017_styles' );
+
+/**
+ * Turn on "new" features for this local site. New features are enabled based on time of site
+ * creation, approximated based on ID number in the wordcamp.org network. That doesn't map to
+ * local sites, so there is difference in markup for some features if we don't whitelist the ID.
+ *
+ * Change this to the Site ID of your local development site (though it will likely be 47 if
+ * this is the first additional site you've created)
+ */
+function boston_2017_enable_features( $sites ) {
+	$sites[] = 47;
+	return $sites;
+}
+add_filter( 'wcpt_speaker_post_avatar_enabled_site_ids',       'boston_2017_enable_features' );
+add_filter( 'wcpt_session_post_speaker_info_enabled_site_ids', 'boston_2017_enable_features' );
+add_filter( 'wcpt_session_post_slides_info_enabled_site_ids',  'boston_2017_enable_features' );
+add_filter( 'wcpt_session_post_video_info_enabled_site_ids',   'boston_2017_enable_features' );
+add_filter( 'wcpt_speaker_post_session_info_enabled_site_ids', 'boston_2017_enable_features' );
